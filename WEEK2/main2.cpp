9bd759e72432f814cccaa799fe4f3fb91c3cf5e2
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-08-27 14:55:43 +0700

    UPDATE WEEK2

--- WEEK2/main2.cpp
+++ WEEK2/main2.cpp
@@ -1,6 +1,247 @@
 #include <iostream>
+#include <cstdio>
 using namespace std; 
 int main() {
-    cout << "Hello, tris nhaan dep troai" << endl;
+    
+    
+
+    // STUDENT 1
+    string ST_N1 = "HUỲNH VÕ TRÍ NHÂN",
+           ST_ID1 = "25AD11005",
+           ST_PHONE1 = "0583432605";
+
+    // STUDENT 2
+    string ST_N2 = "NGUYỄN HOÀNG GIA BẢO",
+           ST_ID2 = "25AD11003",
+           ST_PHONE2 = "0789447376";
+
+    // STUDENT 3
+    string ST_N3 = "ĐIỀN GIA KHIÊM",
+           ST_ID3 = "25AD11006",
+           ST_PHONE3 = "0777777756";
+
+    // STUDENT 4
+    string ST_N4 = "ĐINH THANH HÀO",
+           ST_ID4 = "25AD11007",
+           ST_PHONE4 = "0975940624";
+
+    // STUDENT 5
+    string ST_N5 = "NGUYỄN HOÀNG MAI THANH",
+           ST_ID5 = "25AD14902",
+           ST_PHONE5 = "0708361522";
+
+    // STUDENT 6
+    string ST_N6 = "NGUYỄN PHÚ QUỐC",
+           ST_ID6 = "25AD14904",
+           ST_PHONE6 = "0968282754";
+
+    // STUDENT 7
+    string ST_N7 = "TRẦN TUẤN ĐẠT",
+           ST_ID7 = "25GF14301",
+           ST_PHONE7 = "0827260042";
+
+    // STUDENT 8
+    string ST_N8 = "PHẠM THÁI TRUNG",
+           ST_ID8 = "25GF14303",
+           ST_PHONE8 = "0967056353";
+
+    // STUDENT 9
+    string ST_N9 = "VÕ VĂN TẤN",
+           ST_ID9 = "25TM11001",
+           ST_PHONE9 = "0932509767";
+
+    // STUDENT 10
+    string ST_N10 = "BÙI HIẾU NHÂN",
+           ST_ID10 = "25TM11003",
+           ST_PHONE10 = "0396480948";
+
+    // STUDENT 11
+    string ST_N11 = "NGUYỄN TRƯỜNG THỊNH",
+           ST_ID11 = "25TM14301",
+           ST_PHONE11 = "0913698881";
+
+    // STUDENT 12
+    string ST_N12 = "LẠI NHẬT MINH",
+           ST_ID12 = "25TM14302",
+           ST_PHONE12 = "0867539953";
+
+    // STUDENT 13
+    string ST_N13 = "BÙI VĨNH THỤY",
+           ST_ID13 = "25TM14502",
+           ST_PHONE13 = "0941136060";
+
+    // STUDENT 14
+    string ST_N14 = "HUỲNH NGUYỄN THANH ĐỨC",
+           ST_ID14 = "25TM14503",
+           ST_PHONE14 = "0902952507";
+
+    // STUDENT 15
+    string ST_N15 = "TRẦN ĐỨC VIỆT",
+           ST_ID15 = "25TM14504",
+           ST_PHONE15 = "0708527985";
+
+    // STUDENT 16
+    string ST_N16 = "PHAN THANH DUY",
+           ST_ID16 = "25TM14505",
+           ST_PHONE16 = "0832773244";
+
+    // STUDENT 17
+    string ST_N17 = "PHẠM NGUYỄN ANH KHÔI",
+           ST_ID17 = "25TM14506",
+           ST_PHONE17 = "0785230807";
+
+    // STUDENT 18
+    string ST_N18 = "TRẦN NGỌC THỊNH",
+           ST_ID18 = "25TM14508",
+           ST_PHONE18 = "0941663061";
+
+    // STUDENT 19
+    string ST_N19 = "LÊ NGUYỄN LONG HUY",
+           ST_ID19 = "25TM14509",
+           ST_PHONE19 = "0337385013";
+
+    // STUDENT 20
+    string ST_N20 = "LÊ TRUNG HIẾU",
+           ST_ID20 = "25TM14510",
+           ST_PHONE20 = "0399973179";
+
+
+
+
+    printf("\033[1;33m");
+    printf("======================================\n");
+    printf("             STUDENT LIST             \n");
+    printf("======================================\n");
+    printf("\033[0m");
+
+
+
+    cout << "\033[1;35m" << "Student 1" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N1.c_str());
+    printf("Student ID  : %s\n", ST_ID1.c_str());
+    printf("Phone       : %s\n", ST_PHONE1.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 2" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N2.c_str());
+    printf("Student ID  : %s\n", ST_ID2.c_str());
+    printf("Phone       : %s\n", ST_PHONE2.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 3" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N3.c_str());
+    printf("Student ID  : %s\n", ST_ID3.c_str());
+    printf("Phone       : %s\n", ST_PHONE3.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 4" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N4.c_str());
+    printf("Student ID  : %s\n", ST_ID4.c_str());
+    printf("Phone       : %s\n", ST_PHONE4.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 5" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N5.c_str());
+    printf("Student ID  : %s\n", ST_ID5.c_str());
+    printf("Phone       : %s\n", ST_PHONE5.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 6" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N6.c_str());
+    printf("Student ID  : %s\n", ST_ID6.c_str());
+    printf("Phone       : %s\n", ST_PHONE6.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 7" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N7.c_str());
+    printf("Student ID  : %s\n", ST_ID7.c_str());
+    printf("Phone       : %s\n", ST_PHONE7.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 8" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N8.c_str());
+    printf("Student ID  : %s\n", ST_ID8.c_str());
+    printf("Phone       : %s\n", ST_PHONE8.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 9" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N9.c_str());
+    printf("Student ID  : %s\n", ST_ID9.c_str());
+    printf("Phone       : %s\n", ST_PHONE9.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 10" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N10.c_str());
+    printf("Student ID  : %s\n", ST_ID10.c_str());
+    printf("Phone       : %s\n", ST_PHONE10.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 11" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N11.c_str());
+    printf("Student ID  : %s\n", ST_ID11.c_str());
+    printf("Phone       : %s\n", ST_PHONE11.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 12" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N12.c_str());
+    printf("Student ID  : %s\n", ST_ID12.c_str());
+    printf("Phone       : %s\n", ST_PHONE12.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 13" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N13.c_str());
+    printf("Student ID  : %s\n", ST_ID13.c_str());
+    printf("Phone       : %s\n", ST_PHONE13.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 14" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N14.c_str());
+    printf("Student ID  : %s\n", ST_ID14.c_str());
+    printf("Phone       : %s\n", ST_PHONE14.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 15" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N15.c_str());
+    printf("Student ID  : %s\n", ST_ID15.c_str());
+    printf("Phone       : %s\n", ST_PHONE15.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 16" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N16.c_str());
+    printf("Student ID  : %s\n", ST_ID16.c_str());
+    printf("Phone       : %s\n", ST_PHONE16.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 17" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N17.c_str());
+    printf("Student ID  : %s\n", ST_ID17.c_str());
+    printf("Phone       : %s\n", ST_PHONE17.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 18" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N18.c_str());
+    printf("Student ID  : %s\n", ST_ID18.c_str());
+    printf("Phone       : %s\n", ST_PHONE18.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 19" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N19.c_str());
+    printf("Student ID  : %s\n", ST_ID19.c_str());
+    printf("Phone       : %s\n", ST_PHONE19.c_str());
+    cout << endl;
+
+    cout << "\033[1;35m" << "Student 20" << "\033[0m" << endl;
+    printf("Name        : %s\n", ST_N20.c_str());
+    printf("Student ID  : %s\n", ST_ID20.c_str());
+    printf("Phone       : %s\n", ST_PHONE20.c_str());
+    cout << endl;
+
+    printf("\033[1;33m");
+    printf("======================================\n");
+    printf("Total students: \033[1;31m20\033[0m   \n");
+    printf("\033[1;33m");
+    printf("======================================\n");
+    printf("\033[0m");
+
     return 0;
 }
