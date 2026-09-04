d419ad3c3eda5cea282fae8c17b88c8062e864f8
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-04 17:21:00 +0700

    complete functionn

--- WEEK4/cpp3.cpp
+++ WEEK4/cpp3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 bool kt_nto(int n);
 int main()
@@ -23,7 +25,7 @@ int main()
 bool kt_nto(int n)
 {
 
-    for (int i=0;i<n;i++)
+    for (int i=0;i<=sqrt(n);i++)
     {
         if (n%i ==0)
         {
