eb89c0a9f28efc38395c42a1dfb279881763e66e
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-09 09:19:55 +0700

    xog lab3

--- WEEK5/StudentScoreAnalyzer.cpp
+++ WEEK5/StudentScoreAnalyzer.cpp
@@ -17,17 +17,25 @@ int main() {
   } while (n < 2 || n > 20);
 
   
-  float a;
+  float a[100];
+  a[0]=0;
+
     for (int i=1;i<=n;i++)
     {
         cout << "Enter score for student " << i << ": ";
         
         do {
-            cin >> a;
-            if (a <0 || a>10) cout <<"Invalid score! Pleasee enter a value from 0 to 10: ";
-        } while (a <0 || a>10);
+            cin >> a[i];
+            if (a[i]<0 ||a[i]>10) cout <<"Invalid score! Pleasee enter a value from 0 to 10: ";
+        } while (a[i]<0 || a[i]>10);
     }
 
+    cout << "==== STUDENT SCORES ====\n";
+
+    for (int i=1;i<=n;i++)
+    {
+        cout << "Student " << i << ": " << a[i];
+    }
 
   return 0;
 }
--- WEEK5/main5.cpp
+++ WEEK5/main5.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 int main() {
 
   int n;
   do {
 
-    cout << "how many students ";
+    cout << "how many students? ";
     cin >> n;
     cout << endl;
 
@@ -16,18 +17,26 @@ int main() {
 
   } while (n < 2 || n > 20);
 
-  
-  float a;
-    for (int i=1;i<=n;i++)
-    {
-        cout << "Enter score for student " << i << ": ";
-        
-        do {
-            cin >> a;
-            if (a <0 || a>10) cout <<"Invalid score! Pleasee enter a value from 0 to 10: ";
-        } while (a <0 || a>10);
-    }
+  float a[100];
+  a[0] = 0;
 
+  for (int i = 1; i <= n; i++) {
+    cout << "Enter score for student " << i << ": ";
+
+    do {
+      cin >> a[i];
+      if (a[i] < 0 || a[i] > 10)
+        cout << "Invalid score! Pleasee enter a value from 0 to 10: ";
+    } while (a[i] < 0 || a[i] > 10);
+  }
+
+  cout << endl;
+  cout << "==== STUDENT SCORES ====\n";
+  cout << endl;
+
+  for (int i = 1; i <= n; i++) {
+    printf("Student %d: %.1f\n", i, a[i]);
+  }
 
   return 0;
 }
