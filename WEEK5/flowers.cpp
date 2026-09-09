6f52a72a88fa3182fdc5c6fcfc186b04d536914d
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-09 17:23:54 +0700

    xog cau2

--- WEEK5/flowers.cpp
+++ WEEK5/flowers.cpp
@@ -6,14 +6,18 @@ using namespace std;
 typedef struct {
   int no;
   char name[20];
-  double price;
+  float price;
   int quantity;
   char type[20];
 
 } Flower;
 
 void nhapflowers(Flower DS[], int N);
+
+// câu 1: display all flowers
 void inflowers(Flower DS[], int N);
+// câu 2: find the most expensive flowers
+void max_price(Flower DS[], int N);
 
 int main() {
 
@@ -29,6 +33,7 @@ int main() {
   cin.ignore();
   nhapflowers(flowers, N);
   inflowers(flowers, N);
+  max_price(flowers, N);
 
   return 0;
 }
@@ -54,6 +59,7 @@ void nhapflowers(Flower DS[], int N) {
   cout << endl << endl;
 }
 
+// câu 1: display all flowers
 void inflowers(Flower DS[], int N) {
   printf("====== BANG THONG TIN HOA ======\n");
   printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty", "Type");
@@ -64,3 +70,26 @@ void inflowers(Flower DS[], int N) {
   }
   cout << endl << endl;
 }
+// câu 2: find the most expensive flowers
+void max_price(Flower DS[], int N) {
+  float max = DS[0].price;
+
+  for (int i = 1; i < N; i++) {
+    if (DS[i].price > max) {
+      max = DS[i].price;
+    }
+  }
+
+  printf("====== HOA CO GIA CAO NHAT ======\n");
+  printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty", "Type");
+
+  for (int i = 0; i < N; i++) {
+    if (DS[i].price == max) {
+      printf("%-12d|%-32s|%4.1f|%10d|%20s\n", DS[i].no, DS[i].name, DS[i].price,
+             DS[i].quantity, DS[i].type);
+    }
+  }
+}
+
+
+
