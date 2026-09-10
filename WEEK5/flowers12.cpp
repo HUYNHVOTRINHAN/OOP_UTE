c81836c6d92b857a9de4a74fee78e530dffb54a0
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-11 01:04:51 +0700

    xog bai2

--- WEEK5/flowers12.cpp
+++ WEEK5/flowers12.cpp
@@ -36,6 +36,17 @@ void nhapflowers(Flower DS[], int N) {
   }
   cout << endl << endl;
 }
+// câu 2: display all flowers
+void inflowers(Flower DS[], int N) {
+  printf("====== BANG THONG TIN HOA ======\n");
+  printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty", "Type");
+
+  for (int i = 0; i < N; i++) {
+    printf("%-12d|%-32s|%4.1f|%10d|%20s\n", DS[i].no, DS[i].name, DS[i].price,
+           DS[i].quantity, DS[i].type);
+  }
+  cout << endl << endl;
+}
 
 int main() {
 
@@ -50,6 +61,7 @@ int main() {
 
   cin.ignore();
   nhapflowers(flowers, N);
+  inflowers(flowers, N);
 
   return 0;
 }
