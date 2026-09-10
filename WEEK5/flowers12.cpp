36d235cbf175eaefe5f4a34e8e6ad1952114a4f9
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-11 01:06:27 +0700

    xog bai3

--- WEEK5/flowers12.cpp
+++ WEEK5/flowers12.cpp
@@ -47,6 +47,48 @@ void inflowers(Flower DS[], int N) {
   }
   cout << endl << endl;
 }
+// câu 3: find the most expensive flowers
+void max_price(Flower DS[], int N) {
+  float max = DS[0].price;
+
+  for (int i = 1; i < N; i++) {
+    if (DS[i].price > max) {
+      max = DS[i].price;
+    }
+  }
+
+  printf("====== HOA CO GIA CAO NHAT ======\n");
+  printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty", "Type");
+
+  for (int i = 0; i < N; i++) {
+    if (DS[i].price == max) {
+      printf("%-12d|%-32s|%4.1f|%10d|%20s\n", DS[i].no, DS[i].name, DS[i].price,
+             DS[i].quantity, DS[i].type);
+    }
+  }
+}
+
+// // câu 3: find the cheapest flowers
+// void min_price(Flower DS[], int N) {
+//   float min = DS[0].price;
+
+//   for (int i = 0; i < N; i++) {
+//     if (min > DS[i].price)
+//       min = DS[i].price;
+//   }
+
+//   printf("====== HOA CO GIA THAP NHAT ======\n");
+//   printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty",
+//   "Type");
+
+//   for (int i = 0; i < N; i++) {
+//     if (DS[i].price == min) {
+//       printf("%-12d|%-32s|%4.1f|%10d|%20s\n", DS[i].no, DS[i].name,
+//       DS[i].price,
+//              DS[i].quantity, DS[i].type);
+//     }
+//   }
+// }
 
 int main() {
 
@@ -62,6 +104,7 @@ int main() {
   cin.ignore();
   nhapflowers(flowers, N);
   inflowers(flowers, N);
+  max_price(flowers, N);
 
   return 0;
 }
