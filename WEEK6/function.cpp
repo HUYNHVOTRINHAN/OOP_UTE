51803fbe6ec9eccea553d03d6e095a7f2c4a63e2
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-11 10:16:53 +0700

    step2

--- WEEK6/function.cpp
+++ WEEK6/function.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 // step1
@@ -14,12 +15,30 @@ void showMenu() {
        << "3. Exit" << endl;
   cout << "=====================" << endl;
 }
+void showStudentName(char name[50]) {
+  cout << "Hello, " << name << "!" << endl;
+  cout << "Welcome to our class!" << endl;
+}
+void showStudentID(char ID[50]) {
+  cout << "Enter your Student ID: ";
+  cin.getline(ID, 50);
+  cout << "Student ID: " << ID << endl;
+}
+
 int main() {
 
   showWelcome();
-  cout << "Let's et started!" << endl;
+  cout << "Let's get started!" << endl;
 
   showMenu();
+
+  char name[50];
+  cout << "Enter your name: ";
+  cin.getline(name, 50);
+  showStudentName(name);
+
+  char ID[50];
+  showStudentID(ID);
 
   return 0;
 }
