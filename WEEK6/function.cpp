a02b9aaccac191f27af9df840540264ae8e14f65
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-11 10:53:52 +0700

    step5

--- WEEK6/function.cpp
+++ WEEK6/function.cpp
@@ -1,7 +1,16 @@
 #include <cstdlib>
 #include <iostream>
 #include <string.h>
+
 using namespace std;
+
+typedef struct ScoreInfo {
+  double sum;
+  double avg;
+  int count;
+  double maxScore;
+  double minScore;
+};
 
 // step1
 void showWelcome() {
@@ -16,6 +25,8 @@ void showMenu() {
        << "3. Exit" << endl;
   cout << "=====================" << endl;
 }
+
+// step2
 void showStudentName(char name[50]) {
   cout << "Hello, " << name << "!" << endl;
   cout << "Welcome to our class!" << endl;
@@ -25,6 +36,8 @@ void showStudentID(char ID[50]) {
   cin.getline(ID, 50);
   cout << "Student ID: " << ID << endl;
 }
+
+// step3
 void printStudentInfo(char name[50], int id) {
   cout << "----------------------" << endl;
   cout << "Name: " << name << endl;
@@ -40,7 +53,9 @@ void showScore(char name[50], double &score) {
   cout << "Score  : " << score << endl;
   cout << "Keep going!" << endl;
 }
-double calculateAverage(double score1, double score2) {
+
+// step4
+double calculateavg(double score1, double score2) {
   double sum = score1 + score2;
   double avg = sum / 2.0;
   return avg;
@@ -50,6 +65,38 @@ int maxValue(int a, int b) {
     return b;
   else
     return a;
+}
+
+// step5
+ScoreInfo analyzeScores(double scores[], int n) {
+  ScoreInfo result;
+
+  result.sum = 0;
+  result.count = n;
+
+  if (result.count == 0) {
+    result.avg = 0;
+    result.maxScore = 0;
+    result.minScore = 0;
+    return result;
+  }
+
+  result.minScore = scores[0];
+  result.maxScore = scores[0];
+
+  for (int i = 0; i < n; i++) {
+    result.sum += scores[i];
+
+    if (scores[i] > result.maxScore)
+      result.maxScore = scores[i];
+
+    if (scores[i] < result.minScore)
+      result.minScore = scores[i];
+  }
+
+  result.avg = result.sum / result.count;
+
+  return result;
 }
 
 int main() {
@@ -78,10 +125,19 @@ int main() {
   cin >> math;
   cout << "Enter English score: ";
   cin >> english;
-  double avg = calculateAverage(math, english);
+  double avg = calculateavg(math, english);
   cout << "Avg score = " << avg << endl;
 
   cout << "Mon co diem lon hon: " << maxValue(math, english) << endl;
+
+  double scores[5] = {8.5, 7.0, 9.2, 6.5, 10.0};
+
+  ScoreInfo result = analyzeScores(scores, 5);
+
+  cout << "Sum: " << result.sum << endl;
+  cout << "avg: " << result.avg << endl;
+  cout << "Max: " << result.maxScore << endl;
+  cout << "Min: " << result.minScore << endl;
 
   return 0;
 }
