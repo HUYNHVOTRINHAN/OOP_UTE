89e9fb6b9a3a92071386f7dd07ad64408bbd6549
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-12 11:11:47 +0700

    cau1

---
+++ WEEK6/flowers.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+// kiểu cấu trúc
+typedef struct {
+  int no;
+  char name[20];
+  float price;
+  int quantity;
+  char type[20];
+
+} Flower;
+
+void nhapflowers(Flower DS[], int N) {
+  printf("====== Nhap thong tin hoa ======\n");
+
+  for (int i = 0; i < N; i++) {
+    DS[i].no = i + 1;
+    cout << "Nhap thong tin hoa " << i + 1 << ": " << endl;
+    cout << "Name: ";
+    cin.getline(DS[i].name, 20);
+    cout << "Price: ";
+    cin >> DS[i].price;
+    cin.ignore();
+    cout << "Quantity: ";
+    cin >> DS[i].quantity;
+    cin.ignore();
+    cout << "Type: ";
+    cin.getline(DS[i].type, 20);
+    cout << endl;
+  }
+  cout << endl << endl;
+}
+// câu 1: display all flowers
+void inflowers(Flower DS[], int N) {
+  printf("====== BANG THONG TIN HOA ======\n");
+  printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty", "Type");
+
+  for (int i = 0; i < N; i++) {
+    printf("%-12d|%-32s|%4.1f|%10d|%20s\n", DS[i].no, DS[i].name, DS[i].price,
+           DS[i].quantity, DS[i].type);
+  }
+  cout << endl << endl;
+}
+
+int main() {
+
+  Flower flowers[20];
+  int N;
+  do {
+    cout << "Number of flowers: ";
+    cin >> N;
+    if (N < 1 || N > 20)
+      cout << "please enter the suitable number of flowers from 1 to 20!";
+  } while (N < 1 || N > 20);
+
+  cin.ignore();
+  nhapflowers(flowers, N);
+  inflowers(flowers, N);
+
+  return 0;
+}
