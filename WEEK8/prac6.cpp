343af815af17f640ab601850c1cfa083dc78a0b6
Nhandeptrai <huynhnhan.44.tr4@gmail.com>
2026-09-23 08:16:54 +0700

    prac6

---
+++ WEEK8/prac6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertSubarray(vector<int>& arr, const vector<int>& sub, int index) {
+    arr.insert(arr.begin() + index, sub.begin(), sub.end());
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<int> sub = {9, 9};
+    int pos = 2;
+
+    insertSubarray(nums, sub, pos);
+
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
